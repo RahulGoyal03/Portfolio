644a14a1bd41fbcfd8fd7b67c0ceb48f8f605c1c
Rahul Goyal <91531231+RahulGoyal03@users.noreply.github.com>
2022-01-10 18:13:05 +0530

    Add files via upload

---
+++ src/Components/Contact/Contact.jsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import { ThemeContext } from '../../ContextProvider/ThemeContext';
+import styles from './Contact.module.css';
+
+const Contact = () => {
+	const { newTheme } = React.useContext(ThemeContext);
+	return (
+		<footer
+			className={styles.footer}
+			id='contact'
+			// style={{ background: `${newTheme.highlightBackground}` }}
+		>
+			<div className={styles.container}>
+				<h1 style={{ color: `${newTheme.title}` }}>Get in Touch</h1>
+				<p
+					style={{ color: `${newTheme.para}` }}
+					className={styles.paragraph}
+				>
+					I'm actively looking for any new opportunities, in
+					full-stack web development.
+				</p>
+				<div
+					style={{ color: `${newTheme.title}` }}
+					className={styles.contactOptions}
+				>
+					<a
+						href='tel:9454538373'
+						aria-label='GitHub'
+						rel='noreferrer'
+						target='_blank'
+					>
+						<i className='fas fa-phone-alt' />
+					</a>
+					<a
+						href='mailto: himrd95@gmail.com'
+						aria-label='email'
+						target='_blank'
+						rel='noreferrer'
+					>
+						<i className='far fa-envelope'></i>
+					</a>
+					<a
+						href='https://twitter.com/himansh03285202'
+						aria-label='Twitter'
+						target='_blank'
+						rel='noreferrer'
+					>
+						<i className='fab fa-twitter'></i>
+					</a>
+					<a
+						href='https://github.com/himrd95'
+						aria-label='GitHub'
+						rel='noreferrer'
+						target='_blank'
+					>
+						<i className='fab fa-github' />
+					</a>
+
+					<a
+						href='https://www.linkedin.com/in/himanshu-dwivedi-861205112/'
+						aria-label='Linkedin'
+						target='_blank'
+						rel='noreferrer'
+					>
+						<i className='fab fa-linkedin-in' />
+					</a>
+				</div>
+			</div>
+			<div
+				style={{ background: `${newTheme.line}` }}
+				className={styles.line}
+			/>
+			<div
+				style={{ color: `${newTheme.para}` }}
+				className={styles.copyright}
+			>
+				Designed and build by Himanshu Dwivedi, 2021 All rights
+				reserved.
+			</div>
+		</footer>
+	);
+};
+
+export default Contact;
