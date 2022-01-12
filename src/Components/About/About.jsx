97eaa528e580c2929ac3488c89d8287dbd55ea54
Rahul Goyal <91531231+RahulGoyal03@users.noreply.github.com>
2022-01-12 18:40:11 +0530

    some bug fix

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { ThemeContext } from "../../ContextProvider/ThemeContext";
+import WindowSize from "../../Utils/WindowSize";
+import styles from "./About.module.css";
+
+
+const About = () => {
+  const { newTheme } = React.useContext(ThemeContext);
+  const [width] = WindowSize();
+  const img =
+    width < 650
+      ? "https://github.com/RahulGoyal03/Portfolio/blob/main/public/IMG_3379-removebg-modified_ccexpress__1_-removebg-preview.png?raw=true"
+      : "https://github.com/RahulGoyal03/Portfolio/blob/main/public/developer.gif?raw=true";
+  return (
+    <div
+      className={styles.container}
+      style={{ boxShadow: `3px 3px 5px ${newTheme.line}` }}
+    >
+      <div className={styles.first}>
+        <img
+          src={
+            "https://github.com/RahulGoyal03/Portfolio/blob/main/public/developer.gif?raw=true"
+          }
+          alt=""
+        />
+      </div>
+      <div className={styles.second}>
+        <h1 style={{ color: `${newTheme.title}` }} className={styles.heading}>
+          About Me
+        </h1>
+        <div className={styles.borderBottom} />
+        <p style={{ color: `${newTheme.para}` }} className={styles.aboutMe}>
+          Hello! My name is Rahul Goyal and I enjoy creating things that live on
+          the internet. My interest in web development started back last year
+          when I was trying to edit things on the web — taught me a lot about
+          HTML & CSS!. <br />
+          <br /> Fast Forwarding to today, I built a number of web applications
+          and 4 major projects. Learned a great deal about teamwork, leadership,
+          and communication. After months of rigorous training, here I am
+          <span style={{ color: `#00a0a0` }}>
+            {" "}
+            looking for an opportunity as a full stack web developer
+          </span>
+          .
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
